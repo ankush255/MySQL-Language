67afcc9372e809e9e8b8e25cc50fc012bc2fed7d
ankush25 <ankushthummar25@gmail.com>
2024-03-28 10:52:55 +0530

    27-03-2024

---
+++ Group By/order_by.sql
@@ -0,0 +1,10 @@
+use demo;
+
+select * from client_master order by city;
+
+
+select * from client_master order by city desc;
+
+
+select * from client_master order by State asc;
+
---
+++ Group By/update.sql
@@ -0,0 +1,3 @@
+use demo;
+
+update products_master set Description = 'Pencil' where ProductNo = 'P0001';
